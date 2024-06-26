ad5e74f5c4691c7fbfd06e05c5cca86534327e7a
sathwikreddygv <sathwikreddygv@gmail.com>
2024-06-26 22:15:01 +0530

    ll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,29 @@
+"use client"
 import React from "react";
 import ThemeSwitch from "./ThemeSwitch";
+import useMediaQuery from "./useMediaQuery";
+import { IconActivityHeartbeat, IconAt, IconNfcOff } from "@tabler/icons-react";
+import { BiInfoCircle, BiLaptop } from "react-icons/bi";
+import { BsInfoCircle } from "react-icons/bs";
+import { GoProject } from "react-icons/go";
+import { IoDocument, IoDocumentOutline } from "react-icons/io5";
+import { FcContacts } from "react-icons/fc";
+import { GrContact } from "react-icons/gr";
+import { MdAlternateEmail, MdContactMail, MdEmail, MdMarkEmailRead, MdOutlineEmail } from "react-icons/md";
+import { TfiEmail } from "react-icons/tfi";
+import { TbWriting } from "react-icons/tb";
 
 const Navbar = ({}) => {
+	const isDesktop = useMediaQuery('(min-width: 768px)');
 	return (
-		<div id='sticky-parallax-header' className="sticky left-1/2 -translate-x-1/2  bg-opacity-60 backdrop-blur-[10px] flex items-center w-[40vw] rounded-full px-4 z-20 dark:border-[hsla(0,0%,100%,.3)] dark:border-2 bg-[rgba(0,0,0,.1)] dark:bg-transparent overflow-auto">
-			<div className="flex-1 text-center p-3 font-bold">About</div>
-			<div className="flex-1 text-center p-3 font-bold">Projects</div>
-			<div className="flex-1 text-center p-3 font-bold">Blog</div>
-			<div className="flex-1 text-center p-3 font-bold">Contact</div>
-			<div className="flex-1 flex items-center justify-center"><ThemeSwitch/></div>
-		</div>
+			<div id={isDesktop ? 'sticky-parallax-header' : ''} className="sticky left-1/2 -translate-x-1/2  bg-opacity-60 backdrop-blur-[10px] flex items-center w-[70vw] sm:w-[40vw] rounded-full px-4 z-20 dark:border-[hsla(0,0%,100%,.3)] dark:border-2 bg-[rgba(0,0,0,.1)] dark:bg-transparent overflow-hidden">
+				<div className="flex-1 text-center p-3 font-bold flex items-center justify-center"><BsInfoCircle className="w-4 h-4 mr-1" strokeWidth={1}/> {isDesktop ? 'About' : ''}</div>
+				<div className="flex-1 text-center p-3 font-bold flex items-center justify-center"><BiLaptop className="w-4 h-4 mr-1" strokeWidth={1}/> {isDesktop ? 'Projects' : ''}</div>
+				<div className="flex-1 text-center p-3 font-bold flex items-center justify-center"><TbWriting className="w-4 h-4 mr-1" strokeWidth={3}/> {isDesktop ? 'Blog' : ''}</div>
+				<div className="flex-1 text-center p-3 font-bold flex items-center justify-center"><IconAt className="w-4 h-4 mr-1" strokeWidth={3}/> {isDesktop ? 'Contact' : ''}</div>
+				<div className="flex-1 flex items-center justify-center"><ThemeSwitch/></div>
+			</div>
+
 	)
 }	
 

5ceb6ef36e01863528c0da4b043c5a4e58fcc46e
sathwikreddygv <sathwikreddygv@gmail.com>
2024-06-26 18:53:14 +0530

    ll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,13 +16,13 @@ export default function Home() {
 		<div className="relative z-10">
 			<div className="h-[100vh] relative ">
 				<div className={`text-center absolute text-[36px] sm:text-[64px] w-full flex flex-col items-center justify-center top-[50vh] -translate-y-1/2 ${pacifico.className}`}>
-					<div>Hi. I'm Sathwik.</div>
+					<div>{"Hi. I'm Sathwik."}</div>
 					<div>A Software Developer.</div>
 					<IconChevronsDown id="scroll-icon" className="w-8 sm:w-12 h-8 sm:h-12" stroke={2}/>
 				</div>
 			</div>
 			<div className={`mt-8 px-4 sm:px-28 text-center text-md`}>
-				<div>I'm a Fullstack developer from India. Here, you'll find my projects, blogs and some cool animations :)</div>
+				<div>{"I'm a Fullstack developer from India. Here, you'll find my projects, blogs and some cool animations :)"}</div>
 				<div>Feel free to explore and connect with me. Thanks for visiting!</div>
 			</div>
 			<div className="circle flex items-center justify-center mt-64 mb-64">

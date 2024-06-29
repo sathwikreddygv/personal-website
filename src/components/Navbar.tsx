a8227b63ceec7ddff34abc99721785c6cd574e59
sathwikreddygv <sathwikreddygv@gmail.com>
2024-06-29 16:01:13 +0530

    ll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,25 +19,25 @@ import { GiInfo } from "react-icons/gi";
 const Navbar = ({}) => {
 	const isDesktop = useMediaQuery('(min-width: 768px)');
 	return (
-		isDesktop?
 		<>
-			<div id={'sticky-parallax-header'} style={{backdropFilter:'blur(10px)', WebkitBackdropFilter:'blur(10px)'}} className="sticky left-1/2 -translate-x-1/2  bg-opacity-60 flex items-center w-[40vw] rounded-full px-4 z-20 dark:border-[hsla(0,0%,100%,.3)] dark:border-2 bg-[rgba(0,0,0,.1)] dark:bg-transparent overflow-hidden">
-				<div className="cp flex-1 text-center p-3 font-bold flex items-center justify-center"><BsInfoCircle className="w-4 h-4 mr-1" strokeWidth={1}/> About</div>
+			<div id={'sticky-parallax-header'} style={{backdropFilter:'blur(10px)', WebkitBackdropFilter:'blur(10px)'}} className="hidden sm:flex sticky left-1/2 -translate-x-1/2  bg-opacity-60 items-center w-[40vw] rounded-full px-4 z-20 dark:border-[hsla(0,0%,100%,.3)] dark:border-2 bg-[rgba(0,0,0,.1)] dark:bg-transparent overflow-hidden">
+				<div className="cp flex-1 text-center p-3 font-bold flex items-center justify-center"><GoInfo className="w-4 h-4 mr-1" strokeWidth={1}/> About</div>
 				<div className="cp flex-1 text-center p-3 font-bold flex items-center justify-center"><BiLaptop className="w-4 h-4 mr-1" strokeWidth={1}/> Projects</div>
 				<div className="cp flex-1 text-center p-3 font-bold flex items-center justify-center"><TbWriting className="w-4 h-4 mr-1" strokeWidth={3}/> Blog</div>
 				<div className="cp flex-1 text-center p-3 font-bold flex items-center justify-center"><IconAt className="w-4 h-4 mr-1" strokeWidth={3}/> Contact</div>
 				<div className="cp flex-1 flex items-center justify-center"><ThemeSwitch/></div>
 			</div>
-		</>:
-		<div className="fixed flex items-center justify-center left-0 right-0 bottom-4 m-auto z-20 w-[100vw]">
-			<div style={{backdropFilter:'blur(10px)', WebkitBackdropFilter:'blur(10px)'}} className="bg-opacity-60 flex items-center w-[70vw] rounded-full px-2 py-3 z-20 dark:border-[hsla(0,0%,100%,.3)] dark:border-2 bg-[rgba(0,0,0,.1)] dark:bg-transparent overflow-hidden">
-				<div className="flex-1 text-center font-bold flex items-center justify-center"><GoInfo className="w-6 h-6 mr-2" strokeWidth={1}/></div>
-				<div className="flex-1 text-center font-bold flex items-center justify-center"><BiLaptop className="w-6 h-6 mr-2" strokeWidth={1}/></div>
-				<div className="flex-1 text-center font-bold flex items-center justify-center"><TbWriting className="w-6 h-6 mr-2" strokeWidth={3}/></div>
-				<div className="flex-1 text-center font-bold flex items-center justify-center"><IconAt className="w-6 h-6 mr-2" strokeWidth={3}/></div>
-				<div className="flex-1 flex items-center justify-center"><ThemeSwitch/></div>
+
+			<div className="fixed flex sm:hidden items-center justify-center left-0 right-0 bottom-4 m-auto z-20 w-[100vw]">
+				<div style={{backdropFilter:'blur(10px)', WebkitBackdropFilter:'blur(10px)'}} className="bg-opacity-60 flex items-center w-[70vw] rounded-full px-2 py-3 z-20 dark:border-[hsla(0,0%,100%,.3)] dark:border-2 bg-[rgba(0,0,0,.1)] dark:bg-transparent overflow-hidden">
+					<div className="flex-1 text-center font-bold flex items-center justify-center"><GoInfo className="w-6 h-6 mr-2" strokeWidth={1}/></div>
+					<div className="flex-1 text-center font-bold flex items-center justify-center"><BiLaptop className="w-6 h-6 mr-2" strokeWidth={1}/></div>
+					<div className="flex-1 text-center font-bold flex items-center justify-center"><TbWriting className="w-6 h-6 mr-2" strokeWidth={3}/></div>
+					<div className="flex-1 text-center font-bold flex items-center justify-center"><IconAt className="w-6 h-6 mr-2" strokeWidth={3}/></div>
+					<div className="flex-1 flex items-center justify-center"><ThemeSwitch/></div>
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }	
 
--- src/components/useMediaQuery.tsx
+++ src/components/useMediaQuery.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useMediaQuery = (query) => {
+const useMediaQuery = (query:any) => {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
